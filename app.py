e2b8516d0f0a7c17b1b98ce311ecbd5cf99998d4
farrukh9152 <farrukhhadi00@gmail.com>
2025-12-18 16:06:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    percentage = None
+    result = None
     if request.method == "POST":
-        obtained = float(request.form["obtained"])
-        total = float(request.form["total"])
-        percentage = (obtained / total) * 100
-    return render_template("index.html", percentage=percentage)
+        subjects = ["Maths", "Science", "English", "Computer", "Hindi"]
+        marks = []
+        total = 0
+
+        for sub in subjects:
+            mark = int(request.form.get(sub))
+            marks.append(mark)
+            total += mark
+
+        percentage = total / (len(subjects) * 100) * 100
+
+        result = {
+            "subjects": zip(subjects, marks),
+            "total": total,
+            "percentage": round(percentage, 2)
+        }
+
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
